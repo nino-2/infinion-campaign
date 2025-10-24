46db7cc693df50171c69d65ea50c4326e43d7fc4
Morgan Opeyemi <ayodeleopeyemi09@gmail.com>
2025-10-24 07:17:21 +0100

    fixed root

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Overview from './pages/Overview'
 import Campaign from './pages/Campaign'
 import NewCampaign from './pages/NewCampaign'
 import UserCampaign from './pages/UserCampaign'
+
 
 function App() {
 
   return (
     <>
       <Routes>
+        {/*Root Route*/}
+         <Route path='/' element={<Navigate to='/overview' replace />} />
+
          <Route path='/overview' element={<Overview/>} />
          <Route path='/campaigns' element={<Campaign/>} />
          <Route path='/campaign/new' element={<NewCampaign/>}/>
